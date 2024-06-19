442296c4c2716e69df7765ff55e7e0942ec73e46
Chandan N <138152782+chandanlivo@users.noreply.github.com>
2024-06-19 15:04:47 +0530

    Add files via upload

---
+++ io_stream.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    cout<<"The value of num1 is:\n"; /* this is incertion operator*/
+    cin>>num1;/*this is extraction operator*/
+
+    cout<<"The value of num2 is:\n"; 
+    cin>>num2;
+
+    cout<<"The sum is "<<num1 + num2;
+    
+    return 0;
+}
