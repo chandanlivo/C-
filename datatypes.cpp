e2f88576a5a22b4bd4141388de9314a6a26d6672
Chandan N <138152782+chandanlivo@users.noreply.github.com>
2024-06-19 15:03:46 +0530

    Add files via upload

---
+++ datatypes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+ int glo = 8;
+void mian(){
+    int a ;
+    cout<< glo;
+}
+int main() {
+    int a = 3, b = 2;; 
+    float pi = 3.14;
+    int sum = a +b;
+    glo=88;
+    char c = 'u';
+    cout<< glo;
+    // cout <<  "Here the value of sum is "<< sum<<" \nThe value of pi is "<<pi;
+    // cout<<"\nThis is tutorial 4.\nThe value of a is " <<a<< "\nThe value of b is "<<b;
+    // cout<<"\nThe value of pi is: " << pi;
+    // cout<<"\nThe value of c is: "<<c;
+    return 0;
+}
