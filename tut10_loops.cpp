35e33d34ea4d1a244692e5d4011c55363094739a
Chandan N <138152782+chandanlivo@users.noreply.github.com>
2024-06-21 12:04:14 +0530

    Add files via upload

---
+++ tut10_loops.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //Syntax of FOR  LOOP
+    //for(intialization; condition; updation)
+    // for(int i = 1; i  < 12; i++){
+    //     cout<<i<<endl;
+    // }
+
+    //INFINITE FOR LOOP
+    /*
+    for(int i = 1; 1  < 2; i++){
+       cout<<i<<endl;
+     }
+     */ 
+
+    //Syntax of WHILE loop
+    /*
+    Intialization 
+    While(condition){
+        code
+    }
+    */ 
+     /*int i= 0;
+     while(i<=15){
+         cout<<i<<endl;
+         i++;
+     }
+     */
+    
+
+    //Infinite WHILE loop
+
+    // int i = 0;
+    // while(true){
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+    // 
+
+    //DO_WHILE loop
+    int i = 1;
+    do{
+        cout<<i<<endl;
+        i++;
+    }while(i<=16);
+
+    return 0;
+
+    
+}
